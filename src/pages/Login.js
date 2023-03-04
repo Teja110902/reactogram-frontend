ef88c6d3bc0e3725eee040a84c88f3b79af6f38d
Teja110902 <74037779+Teja110902@users.noreply.github.com>
2023-03-04 18:31:19 +0530

    Updated left side of login form

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import './Login.css'
+import socialDesktop from '../images/social-desktop.png'
+
+const Login = () => {
+    return (
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-7">
+                    <img src={socialDesktop} />
+                </div>
+                <div className="col-md-5 border border-danger">Login form</div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
